4a4a593dc481386f57770c974787d61808d98d1c
Chen Yin <923684815@qq.com>
2025-02-12 14:45:14 +0800

    修复market和template页bug

--- frontend/src/pages/History/History.tsx
+++ frontend/src/pages/History/History.tsx
@@ -1,9 +1,50 @@
-import Navbar from '../../components/Navbar/Navbar'
+import Navbar from "../../components/Navbar/Navbar"
+import Tabbar from "../../components/Tabbar/Tabbar"
+import { use, useEffect, useState } from "react"
+import { useFavoriteStore } from "../../store"
+import takePlace from "../../assets/takePlace.png"
 
 const History = () => {
+  const posts=useFavoriteStore((state) => state.posts)
+  const getPosts=useFavoriteStore((state) => state.getPosts)
+
+  // useEffect(() => {
+  //   getPosts()
+  // }, [])
+
+
   return (
-    <div>
-      <Navbar title="历史" />
+    <div className="history-container">
+      <div className="header">
+        <Navbar title="历史" />
+      </div>
+
+      <div className="content">
+        {
+          posts.map((post) => (
+            <div className='commodity'>
+            <div className='commodity-img'>
+            <img src={post.images[0]?post.images[0]:takePlace} alt="" />
+            </div>
+            <div className='commodity-title'>
+              {post.title}
+            </div>
+            <div className='commodity-bottom'>
+              <div className='commodity-price'>
+                {post.price}
+              </div>
+              <div className='commodity-tag'>
+                {post.tag}
+              </div>
+            </div>      
+            </div>
+          ))
+        }
+      </div>
+
+      <div className="footer">
+        <Tabbar />
+      </div>
     </div>
   )
 }
--- frontend/src/pages/Stars/Stars.tsx
+++ frontend/src/pages/Stars/Stars.tsx
@@ -2,14 +2,15 @@ import Navbar from "../../components/Navbar/Navbar"
 import Tabbar from "../../components/Tabbar/Tabbar"
 import { use, useEffect, useState } from "react"
 import { useFavoriteStore } from "../../store"
+import takePlace from "../../assets/takePlace.png"
 
 const Stars = () => {
   const posts=useFavoriteStore((state) => state.posts)
   const getPosts=useFavoriteStore((state) => state.getPosts)
 
-  // useEffect(() => {
-  //   getPosts()
-  // }, [])
+  useEffect(() => {
+    getPosts()
+  }, [])
 
 
   return (
@@ -23,7 +24,7 @@ const Stars = () => {
           posts.map((post) => (
             <div className='commodity'>
             <div className='commodity-img'>
-              <img src={post.images[0]} alt="" />
+            <img src={post.images[0]?post.images[0]:takePlace} alt="" />
             </div>
             <div className='commodity-title'>
               {post.title}
